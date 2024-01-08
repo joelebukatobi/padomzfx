9d1f71a778a98de74494fd18868aa4c528780725
Daniel <oladotun.olufemi@payhippo.ng>
2024-01-08 12:50:11 +0100

    update navbar

--- src/app/(routes)/layout.jsx
+++ src/app/(routes)/layout.jsx
@@ -4,13 +4,11 @@ import { usePathname } from "next/navigation";
 // Components
 import { Footer } from "@/_components/organisms/Footer";
 import { LandingHeader } from "@/_components/organisms/LandingHeader";
-import TradingViewWidget from "../_components/organisms/TradingView";
 export default function Layout({ children }) {
   //
   const pathname = usePathname();
   return (
     <div className="">
-      <TradingViewWidget />
       {pathname === "/" ? <LandingHeader /> : ""}
       {children}
       <Footer />
--- src/app/_components/organisms/Navbar.jsx
+++ src/app/_components/organisms/Navbar.jsx
@@ -2,9 +2,11 @@
 // React
 import { useEffect, useRef, useState } from "react";
 // Next
+import TradingViewWidget from "@/_components/organisms/TradingView";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+
 // Components
 
 // Image
@@ -47,91 +49,98 @@ export const Navbar = () => {
 
   return (
     <div className="container">
-      <nav className="navbar">
-        <a href="/">
-          <Image width="" height="" src={Logo} alt="Logo" />
-        </a>
-        <div className="navbar-menu lg:hidden">
-          <svg onClick={toggleMenu}>
-            {/* <use href={`/images/sprite.svg#icon-menu`} /> */}
-            <use
-              href={
-                menu
-                  ? "/images/sprite.svg#icon-close"
-                  : "/images/sprite.svg#icon-menu"
-              }
-            />
-          </svg>
-        </div>
-        <ul
-          className={
-            menu
-              ? `flex bg-[#ffffff] h-auto absolute top-[9.6rem] left-0 py-[1.6rem] flex-col w-[100%] rounded-[.4rem] z-10`
-              : `hidden lg:flex`
-          }
-        >
-          <li>
-            <a
-              className={
-                pathname === "/"
-                  ? "text-[#009F0B] font-semibold"
-                  : "text-black font-normal"
-              }
-              href="/"
-            >
-              Home
-            </a>
-          </li>
-          <li aria-label="dropdown" onClick={toggleDropdown} ref={dropdownRef}>
-            <span
-              className={
-                pathname === "/about" || pathname === "/team"
-                  ? "text-[#009F0B] font-semibold"
-                  : "text-black font-normal"
-              }
-            >
-              {" "}
-              Company
-            </span>
-            <hr className={dropdown ? `block` : "hidden"} />
-            <svg>
-              <use href={`/images/sprite.svg#icon-caret`} />
+      <div className="navbar-wrapper">
+        <TradingViewWidget />
+        <nav className="navbar">
+          <a href="/">
+            <Image width="" height="" src={Logo} alt="Logo" />
+          </a>
+          <div className="navbar-menu lg:hidden">
+            <svg onClick={toggleMenu}>
+              {/* <use href={`/images/sprite.svg#icon-menu`} /> */}
+              <use
+                href={
+                  menu
+                    ? "/images/sprite.svg#icon-close"
+                    : "/images/sprite.svg#icon-menu"
+                }
+              />
             </svg>
-            <ul className={dropdown ? `block` : `hidden`}>
-              <li>
-                <Link href="/about">About Us</Link>
-              </li>
-              {/* <li>
+          </div>
+          <ul
+            className={
+              menu
+                ? `flex bg-[#ffffff] h-auto absolute top-[9.6rem] left-0 py-[1.6rem] flex-col w-[100%] rounded-[.4rem] z-10`
+                : `hidden lg:flex`
+            }
+          >
+            <li>
+              <a
+                className={
+                  pathname === "/"
+                    ? "text-[#009F0B] font-semibold"
+                    : "text-black font-normal"
+                }
+                href="/"
+              >
+                Home
+              </a>
+            </li>
+            <li
+              aria-label="dropdown"
+              onClick={toggleDropdown}
+              ref={dropdownRef}
+            >
+              <span
+                className={
+                  pathname === "/about" || pathname === "/team"
+                    ? "text-[#009F0B] font-semibold"
+                    : "text-black font-normal"
+                }
+              >
+                {" "}
+                Company
+              </span>
+              <hr className={dropdown ? `block` : "hidden"} />
+              <svg>
+                <use href={`/images/sprite.svg#icon-caret`} />
+              </svg>
+              <ul className={dropdown ? `block` : `hidden`}>
+                <li>
+                  <Link href="/about">About Us</Link>
+                </li>
+                {/* <li>
                 <Link href="/team">The Team</Link>
               </li> */}
-            </ul>
-          </li>
-          <li>
-            <Link
-              className={
-                pathname === "/training"
-                  ? "text-[#009F0B] font-semibold"
-                  : "text-black font-normal"
-              }
-              href="/training"
-            >
-              Training
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={
-                pathname === "/contact"
-                  ? "text-[#009F0B] font-semibold"
-                  : "text-black font-normal"
-              }
-              href="/contact"
-            >
-              Get In Touch
-            </Link>
-          </li>
-        </ul>
-      </nav>
+              </ul>
+            </li>
+            <li>
+              <Link
+                className={
+                  pathname === "/training"
+                    ? "text-[#009F0B] font-semibold"
+                    : "text-black font-normal"
+                }
+                href="/training"
+              >
+                Training
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={
+                  pathname === "/contact"
+                    ? "text-[#009F0B] font-semibold"
+                    : "text-black font-normal"
+                }
+                href="/contact"
+              >
+                Get In Touch
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
